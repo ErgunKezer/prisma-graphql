e3d37f6ce27dd9973ae866ac37f82795c0b037df
ErgunKezer <40989566+ErgunKezer@users.noreply.github.com>
2020-11-28 19:50:29 +0300

    Paths changed

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ function getUserId(request, connection) {
 }
 
 const server = new GraphQLServer({
-  typeDefs: 'src/schema.graphql',
+  typeDefs: __dirname + '/schema.graphql',
   resolvers,
   context({ request, response, connection }) {
     return {
--- src/prisma.js
+++ src/prisma.js
@@ -1,7 +1,7 @@
 import { Prisma } from 'prisma-binding';
 import { fragmentReplacements } from '@/resolvers';
 const prisma = new Prisma({
-  typeDefs: 'src/generated/prisma.graphql',
+  typeDefs: __dirname + '/generated/prisma.graphql',
   endpoint: process.env.PRISMA_ENDPOINT,
   secret: process.env.PRISMA_SECRET_KEY,
   fragmentReplacements,
